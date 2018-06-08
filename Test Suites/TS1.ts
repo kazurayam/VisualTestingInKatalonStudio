0ecd2b6a9ecf708efe77f541c84c6b2331417f16
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-06-08 13:31:05 +0900

    renamed the project from VisualTestingWithImageMagickAndKatalonStudio to VisualTestingWithKatalonStudio

--- Test Suites/TS1.ts
+++ Test Suites/TS1.ts
@@ -4,7 +4,7 @@
    <name>TS1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-06-08T13:21:55</lastRun>
+   <lastRun>2018-06-08T13:29:04</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
