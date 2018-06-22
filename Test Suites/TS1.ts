1fe1a0e35b4cd5742f41a6fc32c0f3af5a466357
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-06-22 16:47:49 +0900

    now we can follow links to Images from index.html

--- Test Suites/TS1.ts
+++ Test Suites/TS1.ts
@@ -4,7 +4,7 @@
    <name>TS1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-06-22T11:48:47</lastRun>
+   <lastRun>2018-06-22T16:44:05</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
