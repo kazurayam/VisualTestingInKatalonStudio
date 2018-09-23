c9183739c8d3961b6cd6fa5a3d5c3a69e2b15fd1
kazurayam <kazuaki.urayama@gmail.com>
2018-09-23 15:30:00 +0900

    remove toS3.sh

--- toS3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# send ./Materials/* to AWS S3
-
-aws s3 cp --recursive ./Materials/ s3://using-materials-demo/Materials/
-aws s3 cp --recursive ./Reports/   s3://using-materials-demo/Reports/
-
-echo see https://s3-ap-northeast-1.amazonaws.com/using-materials-demo/Materials/index.html#
