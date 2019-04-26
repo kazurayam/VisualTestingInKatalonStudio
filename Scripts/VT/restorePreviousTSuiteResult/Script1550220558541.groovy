94999c2be144a1663dc4130e80219b66a0e97066
kazurayam <kazuaki.urayama@gmail.com>
2019-04-26 17:25:23 +0900

    extended Test Cases/VT/restorePreviousTSuiteResult to have more types of STRATEGIes

--- Scripts/VT/restorePreviousTSuiteResult/Script1550220558541.groovy
+++ Scripts/VT/restorePreviousTSuiteResult/Script1550220558541.groovy
@@ -34,7 +34,27 @@ switch(STRATEGY) {   // is defined in the Variables tab
 		    KeywordUtil.markFailedAndStop("${e.getMessage()}")
 		}
 	    break
-		
+	
+	case '10minutesAgo':
+		// restore the shot of 1 hour ago or older
+		try {
+			LocalDateTime base = LocalDateTime.now().minusMinutes(10)
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '30minutesAgo':
+		// restore the shot of 1 hour ago or older
+		try {
+			LocalDateTime base = LocalDateTime.now().minusMinutes(10)
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
 	case '1hourAgo':
 		// restore the shot of 1 hour ago or older
 	    try {
@@ -44,17 +64,87 @@ switch(STRATEGY) {   // is defined in the Variables tab
 		    KeywordUtil.markFailedAndStop("${e.getMessage()}")
 		}
 		break
-		
-	case '18pmLastEvening':
-	    // restore the shot of last evenng 18:00 or older
-	    try {
-	        LocalDateTime base = LocalDateTime.now().minusDays(1)
-		    count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 18, 0, 0))
-	    } catch (IllegalArgumentException e) {
-		    KeywordUtil.markFailedAndStop("${e.getMessage()}")
+	
+	case '2hoursAgo':
+		// restore the shot of 2 hours ago or older
+		try {
+			LocalDateTime base = LocalDateTime.now().minusHours(2)
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
 		}
 		break
 		
+	case '6amToday':
+	    // restore the shot taken before 6AM today
+	    try {
+		    LocalDateTime base = LocalDateTime.now()
+		    count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 6, 0, 0))
+	    } catch (IllegalArgumentException e) {
+		    KeywordUtil.markFailedAndStop("${e.getMessage()}")
+	    }
+	    break
+		
+	case '9amToday':
+		// restore the shot taken before 6AM today
+		try {
+			LocalDateTime base = LocalDateTime.now()
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 9, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '12amToday':
+		// restore the shot taken before 6AM today
+		try {
+			LocalDateTime base = LocalDateTime.now()
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 12, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '15pmToday':
+		// restore the shot taken before 6AM today
+		try {
+			LocalDateTime base = LocalDateTime.now()
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 15, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '18pmToday':
+		// restore the shot taken before 6AM today
+		try {
+			LocalDateTime base = LocalDateTime.now()
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 18, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '21pmToday':
+		// restore the shot taken before 6AM today
+		try {
+			LocalDateTime base = LocalDateTime.now()
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 21, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
+	case '18pmLastEvening':
+		// restore the shot of last evenng 18:00 or older
+		try {
+			LocalDateTime base = LocalDateTime.now().minusDays(1)
+			count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 18, 0, 0))
+		} catch (IllegalArgumentException e) {
+			KeywordUtil.markFailedAndStop("${e.getMessage()}")
+		}
+		break
+
 	default:
 		KeywordUtil.markFailedAndStop("unknown STRATEGY: ${STRATEGY}")
 } 
