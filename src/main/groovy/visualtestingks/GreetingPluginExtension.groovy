288ed1302d454cb367dc18e16075e318474b4bd7
kazurayam <kazuaki.urayama@gmail.com>
2019-05-15 14:20:11 +0900

    remove <projectDir>/src dir which was accidentally added

--- src/main/groovy/visualtestingks/GreetingPluginExtension.groovy
+++
@@ -1,5 +0,0 @@
-package com.kazurayam.visualtestingks
-
-class GreetingPluginExtension {
-    String message = 'Hello form GreetingPlugin'
-}
--- src/main/groovy/visualtestingks/Tasks.groovy
+++
@@ -1,8 +0,0 @@
-package com.kazurayam.visualtestingks
-
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.TaskAction
-
-class Tasks extends DefaultTask {
-}
