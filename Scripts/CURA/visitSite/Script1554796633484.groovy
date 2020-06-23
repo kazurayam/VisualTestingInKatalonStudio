b0eba20b9a1ebb674de7706762baccb38a063d2f
kazurayam <kazuaki.urayama@gmail.com>
2020-06-23 09:51:26 +0900

    add a keyword class MemoryUsageResearch

--- Scripts/CURA/visitSite/Script1554796633484.groovy
+++ Scripts/CURA/visitSite/Script1554796633484.groovy
@@ -52,28 +52,8 @@ WebUI.comment("saved image into ${png1}")
 
 
 
-
-
 // for research, create additional 300 screenshot files
-// take 10 times more of screenshots
-for (int i = 0; i < 10; i++) {
-	WebUI.navigateToUrl(url.toExternalForm())
-	WebUI.verifyElementPresent(findTestObject('CURA/Page_Homepage/a_Make Appointment'),
-			10, FailureHandling.STOP_ON_FAILURE)
-	Path png2 = mr.resolveMaterialPath(GlobalVariable[MGV.CURRENT_TESTCASE_ID.getName()], "トップ${String.format('%03d',i)}.png")
-	CustomKeywords.'com.kazurayam.ksbackyard.ScreenshotDriver.takeEntirePage'(driver, png2.toFile(), 100)
-	WebUI.comment("saved image into ${png2}")
-	WebUI.delay(1)
-}
-Random rand = new Random()
-for (int i = 10; i < 300; i++) {
-	// copy scrennshots to add 290 more times while picking up the source randomly amongst the first 10
-	int x = rand.nextInt(10)
-	Path origin = mr.resolveMaterialPath(GlobalVariable[MGV.CURRENT_TESTCASE_ID.getName()], "トップ${String.format('%03d',x)}.png")
-	Path target = mr.resolveMaterialPath(GlobalVariable[MGV.CURRENT_TESTCASE_ID.getName()], "トップ${String.format('%03d',i)}.png")
-	Files.copy(origin, target, StandardCopyOption.REPLACE_EXISTING)
-	WebUI.comment("saved copy into ${target}")
-}
+com.kazurayam.visualtesting.MemoryUsageResearch.injectFixtures(mr, driver, url)
 
 
 
