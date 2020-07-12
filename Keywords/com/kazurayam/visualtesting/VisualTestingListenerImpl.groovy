5352c7001d475a1280fc9ed9a2ec9a4617dbf76f
kazurayam <kazuaki.urayama@gmail.com>
2020-07-12 12:08:18 +0900

    VisualTestingListenerImpl#beforeTestSuite() displays messages: before/after creating MaterialStorage

--- Keywords/com/kazurayam/visualtesting/VisualTestingListenerImpl.groovy
+++ Keywords/com/kazurayam/visualtesting/VisualTestingListenerImpl.groovy
@@ -150,6 +150,7 @@ public class VisualTestingListenerImpl {
 		Files.createDirectories(materialsDir)
 
 		// create the MaterialRepository object, save it as a GlobalVariable
+		KeywordUtil.logInfo("${hd} creating MaterialRepository")
 		MaterialRepository mr = MaterialRepositoryFactory.createInstance(materialsDir)
 		mr.markAsCurrent(testSuiteId, executionProfile, testSuiteTimestamp)
 		def tsr = mr.ensureTSuiteResultPresent(testSuiteId, executionProfile, testSuiteTimestamp)
@@ -159,22 +160,33 @@ public class VisualTestingListenerImpl {
 		VisualTestingLogger vtLogger4Repos = new VisualTestingLoggerDefaultImpl()
 		mr.setVisualTestingLogger(vtLogger4Repos)
 		GVH.ensureGlobalVariable(MGV.MATERIAL_REPOSITORY, mr)
+		KeywordUtil.logInfo("${hd} created MaterialRepository")
+
 
 		// create the MaterialStorage object, save it as a GlobalVariable
+		KeywordUtil.logInfo("${hd} creating MaterialStorage")
 		Files.createDirectories(storageDir)
 		MaterialStorage ms = MaterialStorageFactory.createInstance(storageDir)
 		VisualTestingLogger vtLogger4Storage = new VisualTestingLoggerDefaultImpl()
 		ms.setVisualTestingLogger(vtLogger4Storage)
 		GVH.ensureGlobalVariable(MGV.MATERIAL_STORAGE, ms)
+		KeywordUtil.logInfo("${hd} created MaterialStorage")
+
 
 		// create the ReportsAccessor object, save it as a GlobalVariable
+		KeywordUtil.logInfo("${hd} creating ReportsAccessor")
 		ReportsAccessor ra = ReportsAccessorFactory.createInstance(reportsDir)
 		GVH.ensureGlobalVariable(MGV.REPORTS_ACCESSOR, ra)
+		KeywordUtil.logInfo("${hd} created ReportsAccessor")
+
 
 		// read TestSuiteId and ExecutionProfile of the last executed test suite
 		// from GlobalVariables.json file
 		if (Files.exists(GLOBAL_VARIABLES_JSON)) {
-			List<String> names = [MGV.LAST_EXECUTED_TESTSUITE_ID.getName(), MGV.LAST_APPLIED_EXECUTION_PROFILE.getName()]
+			List<String> names = [
+				MGV.LAST_EXECUTED_TESTSUITE_ID.getName(),
+				MGV.LAST_APPLIED_EXECUTION_PROFILE.getName()
+			]
 			Reader reader = new InputStreamReader(new FileInputStream(GLOBAL_VARIABLES_JSON.toFile()), "utf-8")
 			Map<String, Object> gvs = GlobalVariableHelpers.read(names, reader)
 			for (String name in gvs.keySet()) {
@@ -294,7 +306,10 @@ public class VisualTestingListenerImpl {
 
 		//
 		Files.createDirectories(GLOBAL_VARIABLES_JSON.getParent())
-		List<String> names = [MGV.LAST_EXECUTED_TESTSUITE_ID.getName(), MGV.LAST_APPLIED_EXECUTION_PROFILE.getName()]
+		List<String> names = [
+			MGV.LAST_EXECUTED_TESTSUITE_ID.getName(),
+			MGV.LAST_APPLIED_EXECUTION_PROFILE.getName()
+		]
 		Writer writer = new OutputStreamWriter(new FileOutputStream(GLOBAL_VARIABLES_JSON.toFile()), "utf-8")
 		GVH.write(names, writer)
 	}
